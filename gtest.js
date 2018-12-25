3535f2941ca83c6fbbdf73306a7e5ddca91c8ec3
Glitch (create-react-app-demo) <none>
2018-12-25 03:55:13 +0000

    🏦🚲 Checkpoint ./package.json:910645/1854 ./gtest.js:910645/839

--- gtest.js
+++ gtest.js
@@ -1,6 +1,6 @@
 'use strict'
 
-console.log('ohey, have process.env.CLIENT_EMAIL?', process.env.CLIENT_EMAIL)
+require('dotenv').config()
 const { google } = require('googleapis')
 
 const scopes = [
@@ -14,10 +14,9 @@ const jwt = new google.auth.JWT(
   process.env.PRIVATE_KEY,
   scopes
 )
-// const view_id = 'XXXXXXX'
-const spreadsheetId = '1hmEYFZlpf0GV1kG2lZophXwzUSKL20v6nDycJVElAcs'
-const range = 'Sheet1!A1:D1'
 
+const spreadsheetId = '1hmEYFZlpf0GV1kG2lZophXwzUSKL20v6nDycJVElAcs'
+const range = 'Sheet1!A1:Z1'
 
 jwt.authorize((err, response) => {
   
@@ -30,25 +29,16 @@ jwt.authorize((err, response) => {
     valueInputOption: 'USER_ENTERED',
     requestBody: {
       values: [
-        ['foo', 'bar', 'baz'],
-        ['bl33p', 'zomg', 'zork'],
+        [Math.random(), 'foo', 'bar', 'baz', 'bl33p', 'zomg', 'zork'],
       ],
     },
   }, (err, result) => { 
-    console.log('err:', err, ' result:', result)
+    if(err){
+      console.log('o noz! error!')
+      console.log(err)
+    }else{
+      console.log('success!') 
+    }
   });
-  console.log('res.data:', res.data);
-  
-  // google.analytics('v3').data.ga.get(
-  //   {
-  //     auth: jwt,
-  //     ids: 'ga:' + view_id,
-  //     'start-date': '30daysAgo',
-  //     'end-date': 'today',
-  //     metrics: 'ga:pageviews'
-  //   },
-  //   (err, result) => {
-  //     console.log(err, result)
-  //   }
-  // )
+
 })
