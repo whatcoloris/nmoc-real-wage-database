155f8b60eecae96840bc60d46717ca62270abba1
Glitch (create-react-app-demo) <none>
2018-12-25 03:44:14 +0000

    🏀🖖 Checkpoint ./gtest.js:910645/2063

--- gtest.js
+++ gtest.js
@@ -1,5 +1,6 @@
 'use strict'
 
+console.log('ohey, have process.env.CLIENT_EMAIL?', process.env.CLIENT_EMAIL)
 const { google } = require('googleapis')
 
 const scopes = [
@@ -13,75 +14,41 @@ const jwt = new google.auth.JWT(
   process.env.PRIVATE_KEY,
   scopes
 )
-const view_id = 'XXXXXXX'
+// const view_id = 'XXXXXXX'
+const spreadsheetId = '1hmEYFZlpf0GV1kG2lZophXwzUSKL20v6nDycJVElAcs'
+const range = 'Sheet1!A1:D1'
+
 
 jwt.authorize((err, response) => {
   
-  google.sheets({
+  const res = google.sheets({
     version: 'v4',
-    auth: sampleClient.oAuth2Client,
-  });
-  
-  google.analytics('v3').data.ga.get(
-    {
-      auth: jwt,
-      ids: 'ga:' + view_id,
-      'start-date': '30daysAgo',
-      'end-date': 'today',
-      metrics: 'ga:pageviews'
-    },
-    (err, result) => {
-      console.log(err, result)
-    }
-  )
-})
-
-
-
-
-
-
-'use strict';
-
-const {google} = require('googleapis');
-// const sampleClient = require('../sampleclient');
-
-const sheets = google.sheets({
-  version: 'v4',
-  auth: sampleClient.oAuth2Client,
-});
-
-async function runSample(spreadsheetId, range) {
-  const res = await sheets.spreadsheets.values.append({
+    auth: jwt,
+  }).spreadsheets.values.append({
     spreadsheetId,
     range,
     valueInputOption: 'USER_ENTERED',
     requestBody: {
       values: [
-        ['Justin', '1/1/2001', 'Website'],
-        ['Node.js', '2018-03-14', 'Fun'],
+        ['foo', 'bar', 'baz'],
+        ['bl33p', 'zomg', 'zork'],
       ],
     },
+  }, (err, result) => { 
+    console.log('err:', err, ' result:', result)
   });
-  console.log(res.data);
-  return res.data;
-}
-
-const scopes = [
-  'https://www.googleapis.com/auth/drive',
-  'https://www.googleapis.com/auth/drive.file',
-  'https://www.googleapis.com/auth/spreadsheets',
-];
-
-if (module === require.main) {
-  const [spreadsheetId, range] = process.argv.slice(2);
-  sampleClient
-    .authenticate(scopes)
-    .then(() => runSample(spreadsheetId, range))
-    .catch(console.error);
-}
-
-module.exports = {
-  runSample,
-  client: sampleClient.oAuth2Client,
-};
+  console.log('res.data:', res.data);
+  
+  // google.analytics('v3').data.ga.get(
+  //   {
+  //     auth: jwt,
+  //     ids: 'ga:' + view_id,
+  //     'start-date': '30daysAgo',
+  //     'end-date': 'today',
+  //     metrics: 'ga:pageviews'
+  //   },
+  //   (err, result) => {
+  //     console.log(err, result)
+  //   }
+  // )
+})
