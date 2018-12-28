54f577c61b425e44be36e52f64277bcb1f9a36e5
Glitch (create-react-app-demo) <none>
2018-12-28 05:57:37 +0000

    🐺🍃 Checkpoint ./jsonouttest.js:910645/2502

--- jsonouttest.js
+++ jsonouttest.js
@@ -4,29 +4,50 @@ const s3 = new aws.S3({
   endpoint: spacesEndpoint
 });
 
+s3.listObjects({Bucket:'emergencyindex'}).
+on('success', function handlePage(item) {
+  item.data.Contents.forEach( item => {
+    if(item.Key.match(/.json/)){
+      s3.getObject({
+       Bucket: 'emergencyindex', 
+       Key: item.Key
+      }, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(JSON.parse(data.Body));           // successful response  
+      });
+    }
+  })
+  if(item.hasNextPage()) {
+    // There's another page; handle it
+    item.nextPage().on('success', handlePage).send();
+  }
+}).on('error', function(err) {
+    console.error('o noz error:',err)
+}).send();
 
-s3.listObjects({
-Bucket: 'emergencyindex', 
-Prefix: 'uploads/',
-MaxKeys: 1000
-}, function(err, data) {
- if (err) {
-   // an error occurred
-   console.log(err, err.stack);
- } else {
-   console.log(data);           // successful response
-   data.Contents.forEach( item => {
-     item.Key
-     s3.getObject({
-      Bucket: 'emergencyindex', 
-      Key: item.Key
-     }, function(err, data) {
-       if (err) console.log(err, err.stack); // an error occurred
-       else     console.log(data);           // successful response
+
+// s3.listObjects({
+// Bucket: 'emergencyindex', 
+// Prefix: 'uploads/',
+// MaxKeys: 1000
+// }, function(err, data) {
+//  if (err) {
+//    // an error occurred
+//    console.log(err, err.stack);
+//  } else {
+//    console.log(data);           // successful response
+//    data.Contents.forEach( item => {
+//      item.Key
+//      s3.getObject({
+//       Bucket: 'emergencyindex', 
+//       Key: item.Key
+//      }, function(err, data) {
+//        if (err) console.log(err, err.stack); // an error occurred
+//        else     console.log(data);           // successful response
        
-     });
+//      });
 
 
-   })
- }
-});
+//    })
+//  }
+// });
