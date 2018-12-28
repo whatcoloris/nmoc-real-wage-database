0a1e453921fc2612b5e89d79b7520ff010d2edd7
Glitch (create-react-app-demo) <none>
2018-12-28 06:08:43 +0000

    🚅🕹 Checkpoint ./jsonouttest.js:910645/825

--- jsonouttest.js
+++ jsonouttest.js
@@ -3,6 +3,7 @@ const spacesEndpoint = new aws.Endpoint('sfo2.digitaloceanspaces.com');
 const s3 = new aws.S3({
   endpoint: spacesEndpoint
 });
+let out_data = [];
 
 s3.listObjects({Bucket:'emergencyindex'}).
 on('success', function handlePage(item) {
@@ -12,8 +13,15 @@ on('success', function handlePage(item) {
        Bucket: 'emergencyindex', 
        Key: item.Key
       }, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(JSON.parse(data.Body));           // successful response  
+        if (err){
+          console.warn(err, err.stack);
+        } else {
+          const project_form = JSON.parse(data.Body);
+          console.log('project_form.data', project_form.data)
+          if (project_form.data && project_form.data.length > 0){
+            out_data.push(project_form.data)
+          }
+        }
       });
     }
   })
@@ -22,9 +30,10 @@ on('success', function handlePage(item) {
     item.nextPage().on('success', handlePage).send();
   }
 }).on('error', function(err) {
-    console.error('o noz error:',err)
+    console.warn('o noz error:',err)
+}).on('complete', function() {
+  console.log('out_data:', out_data);
 }).send();
-
 
 // s3.listObjects({
 // Bucket: 'emergencyindex', 
