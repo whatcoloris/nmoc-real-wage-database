2934db3bef3e8e728ec441f9e6f66cfaa6729bcd
Glitch (create-react-app-demo) <none>
2019-01-01 22:07:48 +0000

    🌻💃 Checkpoint ./jsonouttest.js:910645/4648

--- jsonouttest.js
+++ jsonouttest.js
@@ -3,43 +3,61 @@ const spacesEndpoint = new aws.Endpoint('sfo2.digitaloceanspaces.com');
 const s3 = new aws.S3({
   endpoint: spacesEndpoint
 });
-let promises = [];
 
-s3.listObjects({Bucket:'emergencyindex'}).
-on('success', function handlePage(item) {
+let keys = [];
+
+s3.listObjects({Bucket:'emergencyindex'})
+.on('success', function handlePage(item) {
   item.data.Contents.forEach(function(item) {
     if(item.Key.match(/.json/)){
-      s3.getObject({
-       Bucket: 'emergencyindex', 
-       Key: item.Key
-      }, function(err, data) {
-        promises.push(
-          new Promise(function(resolve, reject) {
-            if (err){
-              console.warn(err, err.stack);
-              reject(err);
-            } else {
-              const project_form = JSON.parse(data.Body);
-              // console.log('project_form.data', project_form.data)
-              if (project_form.data && project_form.data.length > 0){
-                resolve(project_form.data);
-              }
-            }
-          })
-        );
-      });
+      keys.push(item.Key);
     }
   })
   if(item.hasNextPage()) {
-    // There's another page; handle it
+    // another page: handle it
     item.nextPage().on('success', handlePage).send();
   }
 }).on('error', function(err) {
     console.warn('o noz error:',err)
 }).on('complete', function() {
-  console.log('complete!');
+  console.log('complete! keys:', keys);
+  getData();
 }).send();
 
-Promise.all(promises).then( function(data) {
-  setTimeout(function(){ console.log('promises data:', data); }, 1000)
-});
+
+function getData(){
+  let promises = [];
+  keys.forEach( function(k) {
+    promises.push(
+      new Promise(function(resolve, reject) {
+        s3.getObject({
+         Bucket: 'emergencyindex', 
+         Key: k
+        }, function(err, data) {
+          if (err){
+            console.warn(err, err.stack);
+            resolve();
+          } else {
+            const project_form = JSON.parse(data.Body);
+            // console.log('project_form.data', project_form.data)
+            if (project_form.data && project_form.data.length > 0){
+              resolve(project_form.data);
+            }else{
+              resolve();
+            }
+          }
+        });
+      })
+    );
+    
+  })
+  
+  Promise.all(promises).then( function(data) {
+    console.log('promises data:', data);
+  });
+}
+
+
+
+
+
